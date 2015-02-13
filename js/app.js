8e18809b25e498162f4e97fa334ccbb5afdb5dad
Kai <kchisd@rit.edu>
2015-02-13 12:44:43 -0500

    Angular directive for the branding area.

--- js/app.js
+++ js/app.js
@@ -4,23 +4,35 @@
 
 (function(angular) {
 
-angular.module('ewa.components', ['ewa.components.tpls', 'ewa.components.ritheader']);
-angular.module('ewa.components.tpls', ['templates/ritheader/ritheader.html']);
+angular.module('ewa.components', ['ewa.components.tpls', 'ewa.components.ritHeader', 'ewa.components.ritBranding']);
+angular.module('ewa.components.tpls', ['templates/rit-header/rit-header.html', 'templates/rit-branding/rit-branding.html']);
 
-angular.module('ewa.components.ritheader', [])
-.directive('ritheader', function() {
+angular.module('ewa.components.ritHeader', [])
+.directive('ritHeader', function() {
   return {
     restrict: 'E',
-    templateUrl: 'templates/ritheader/ritheader.html'
+    templateUrl: 'templates/rit-header/rit-header.html'
   };
 });
 
-angular.module('templates/ritheader/ritheader.html', []).run(['$templateCache', function($templateCache) {
-  $templateCache.put('templates/ritheader/ritheader.html',
+angular.module('ewa.components.ritBranding', [])
+.directive('ritBranding', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/rit-branding/rit-branding.html',
+    scope: {
+      title: '@ritBrandingTitle',
+      subtitle: '@ritBrandingSubtitle'
+    }
+  };
+});
+
+angular.module('templates/rit-header/rit-header.html', []).run(['$templateCache', function($templateCache) {
+  $templateCache.put('templates/rit-header/rit-header.html',
     '<div class="ritheader navbar navbar-inverse">' +
       '<div class="container">' +
         '<div class="navbar-header">' +
-          '<a class="" href="http://www.rit.edu/"><img src="dist/images/rit-logo-orange.jpg" alt="Rochester Institute of Technology"></a>' +
+          '<a class="navbar-brand" href="http://www.rit.edu/"><img src="dist/images/rit-logo-orange.jpg" alt="Rochester Institute of Technology"></a>' +
         '</div>' +
         '<div class="collapse navbar-collapse">' +
           '<ul class="nav navbar-nav navbar-right">' +
@@ -29,6 +41,19 @@ angular.module('templates/ritheader/ritheader.html', []).run(['$templateCache', 
             '<li><a href="http://www.rit.edu/">RIT Home</a></li>' +
           '</ul>' +
         '</div>' +
+      '</div>' +
+    '</div>'
+  );
+}]);
+
+angular.module('templates/rit-branding/rit-branding.html', []).run(['$templateCache', function($templateCache){
+  $templateCache.put('templates/rit-branding/rit-branding.html',
+    '<div class="branding-wrapper">' +
+      '<div class="branding container">' +
+        '<h1 class="branding-logo">' +
+          '<a href="/">{{title}}</a> ' +
+          '<small>{{subtitle}}</small>' +
+        '</h1>' +
       '</div>' +
     '</div>'
   );
